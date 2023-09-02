4ac4168f3b256082f7f5b26d28ec90b4e4ee0fd3
SumitSawant1029 <123000840+SumitSawant1029@users.noreply.github.com>
2023-09-02 14:24:38 +0530

    Added lowercase feature

--- taur-app/src/component/Textform.js
+++ taur-app/src/component/Textform.js
@@ -2,23 +2,34 @@ import React ,{useState} from 'react'
 
 
 export default function Textform(props){
-    const [Text , setText] = useState('Enter Text here ');
+    const [Text , setText] = useState('Enter Text here');
     const handler = (event) => {
         setText(event.target.value);
     }
     
-    const handler1 =()=>{
+    const handler1L =()=>{
         setText(Text.toUpperCase());
     }
+    const handler1U =()=>{
+        setText(Text.toLowerCase());
+    }
     return(
-
-        <div>
-            <div classname ="container">
-                <h1>{props.heading}</h1>
-                <textarea classname ="form-control" value={Text} onChange={handler} id="exampleFormControlTextare a1" rows="8" ></textarea>
+        <>
+            
+            <div className='container'>
+                <textarea style={{ width: '90%' }} value={Text} onChange={handler} id="exampleFormControlTextare a1" rows="18" ></textarea><br></br><br></br>
+                <button className="btn btn-primary mx-2" onClick={handler1L} >Convert To Upper Case</button>
+                <button className="btn btn-primary mx-2" onClick={handler1U} >Convert To Lower Case</button>
+            
+                <div>
+                    {Text.length} Characters , {Text.split(" ").length} Words<br/>
+                    {0.008 * Text.split("").length} Minutes Read
+                </div>
+                <div>
+                    <h3>Preview</h3>
+                    <p>{Text}</p>
+                </div>
             </div>
-            <button classname="btn btn-primary" onClick={handler1} >Convert To Upper Case</button>
-        </div>
-        
+        </>
     )
 }
