6a34bfaf445a59e9f3196fb7b5929d76dc35d68d
SumitSawant1029 <123000840+SumitSawant1029@users.noreply.github.com>
2023-09-03 09:49:10 +0530

    Done with Dark mode

--- taur-app/src/App.js
+++ taur-app/src/App.js
@@ -2,10 +2,26 @@
 import './App.css';
 import Navbar from './component/Navbar';
 import Textform from './component/Textform';
+import React ,{useState} from 'react'
+
 function App() {
+  const [mode1,setmode1]=useState("light");
+    const toggle = () => {
+       
+        if (mode1 === 'dark'){
+            setmode1('light');
+            document.body.style.backgroundColor='white'
+        }
+        else{
+            setmode1('dark')
+            document.body.style.backgroundColor='grey'
+        }
+
+
+    }
   return (
     <>
-    <Navbar tittle='Convert Your Text Freely'/>
+    <Navbar mode1={mode1} toggle1={toggle}/>
     <Textform heading="Place Your Text Here:-"/>
 
     </>
--- taur-app/src/component/Navbar.js
+++ taur-app/src/component/Navbar.js
@@ -3,21 +3,10 @@ import React , {useState} from 'react'
 
 
 export default function Navbar(props){
-    const [mode1,setmode1]=useState("light");
-    const toggle = () => {
-        console.log("Toggle funtion Called")
-        if (mode1 === 'dark'){
-            setmode1('light');
-        }
-        else{
-            setmode1('dark')
-        }
-
-
-    }
+    
     return(
     <>
-        <nav className={`navbar navbar-expand-lg navbar-${mode1} bg-${mode1}`}>
+        <nav className={`navbar navbar-expand-lg navbar-${props.mode1} bg-${props.mode1}`}>
         <div className="container-fluid">
             <a className="navbar-brand" href="/">TAUR</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -33,7 +22,7 @@ export default function Navbar(props){
                 </li>
                 </ul>
                 <div class="form-check form-switch mx-3">
-                    <input className="form-check-input" onChange={toggle} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                    <input className="form-check-input" onChange={props.toggle1} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
                     <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark mode</label>
                 </div>
                 <form className="d-flex" role="search">
