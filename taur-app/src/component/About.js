5e4e97eb2a1ae7467aa19baf54463eaa12d9d1db
SumitSawant1029 <123000840+SumitSawant1029@users.noreply.github.com>
2023-09-03 09:26:00 +0530

    Dark mode Feature Added

---
+++ taur-app/src/component/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        Hello Bhai This Website is use for practice purpose 
+    <button></button>  
+    </div>
+  )
+}
--- taur-app/src/component/Navbar.js
+++ taur-app/src/component/Navbar.js
@@ -1,10 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React , {useState} from 'react'
+
+
 
 export default function Navbar(props){
+    const [mode1,setmode1]=useState("light");
+    const toggle = () => {
+        console.log("Toggle funtion Called")
+        if (mode1 === 'dark'){
+            setmode1('light');
+        }
+        else{
+            setmode1('dark')
+        }
+
+
+    }
     return(
     <>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className={`navbar navbar-expand-lg navbar-${mode1} bg-${mode1}`}>
         <div className="container-fluid">
             <a className="navbar-brand" href="/">TAUR</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,24 +32,20 @@ export default function Navbar(props){
                 <a className="nav-link" href="/">About</a>
                 </li>
                 </ul>
+                <div class="form-check form-switch mx-3">
+                    <input className="form-check-input" onChange={toggle} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                    <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark mode</label>
+                </div>
                 <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success" type="submit">Search</button>
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button className="btn btn-outline-success" type="submit">Search</button>
                 </form>
             </div>
         </div>
       </nav>
         <div className='container'>
-        <br/>
-        <h4>{props.tittle}</h4>        
+        <br/>       
         </div>
     </>
     )
-}
-Navbar.propTypes = {tittle : PropTypes.string.isRequired,
-                        apptext : PropTypes.string}
-
-Navbar.defaultProps = {
-    tittle: 'Set title here',
-    apptext: 'About Text here'
-}
+};
